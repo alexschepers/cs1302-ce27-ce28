92660c84a99d26e17a13c68898bbb49d9c859cf7
Alex Schepers <ans91118@uga.edu>
2020-04-24 16:09:22 -0400

    checkpoint

--- src/main/java/cs1302/sorting/BubbleSort.java
+++ src/main/java/cs1302/sorting/BubbleSort.java
@@ -1,5 +1,7 @@
 package cs1302.sorting;
 
+import java.util.*;
+
 /**
  * Bubble Sort class.
  */
@@ -7,8 +9,42 @@ public class BubbleSort {
 
     public static void main(String[] args) {
 
-        //implement later
+        Integer [] array = {2, 1, 4, 5, 3, 10};
+        System.out.println("Original Integer array: " + Arrays.toString(array));
+        bubble(array, 0, 4, Integer::compareTo);
+        System.out.println("Testing using just bubble method: " + Arrays.toString(array));
+
+        String[] stringArray = {"hey", "yo", "sup", "hello"};
+        System.out.println("Original String Array: " + Arrays.toString(stringArray));
+        Comparator<String> lengthComp = ((a, b) ->
+                a.length() - b.length()
+                    );
+        bubble(stringArray, 0, 4, lengthComp);
+        System.out.println("comparing by length with just bubble method: "
+                           + Arrays.toString(stringArray));
 
     } // main
+
+    /**
+     * Bubble helper method for bubble sort.
+     *
+     * @param <T>
+     * @param array
+     * @param lo
+     * @param hi
+     * @param c
+     */
+    public static <T> void bubble(T[] array, int lo, int hi, Comparator<T> c) {
+
+        for (int i = lo; i < hi - 1; i++) {
+            if (c.compare(array[i], array[i + 1]) > 0) {
+                T temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            } // if
+
+        } // for
+
+    } // bubble
 
 } // BubbleSort
