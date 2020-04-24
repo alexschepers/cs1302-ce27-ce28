3d8a7b36d30db6988d2e0a6ff018317193623047
Matthew Riddle <yd7fa15@uga.edu>
2020-04-24 15:34:48 -0400

    updated SelectionSort.java

--- src/main/java/cs1302/sorting/SelectionSort.java
+++ src/main/java/cs1302/sorting/SelectionSort.java
@@ -1,1 +1,15 @@
+package cs1302.sorting;
 
+/** Select Sort class.
+ *
+ */
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        //implement later
+
+    } // main
+
+} // SelectionSort
